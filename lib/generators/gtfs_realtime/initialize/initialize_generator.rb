a1063cb81ebf0ea99e22f0c79a2f76c47fe10ee6
mathmerized <mathmerized@gmail.com>
2019-07-30 12:36:50 -0400

    add generator to create cron jobs for refreshing RT feeds

---
+++ lib/generators/gtfs_realtime/initialize/initialize_generator.rb
@@ -0,0 +1,27 @@
+module GtfsRealtime
+  class InitializeGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
+
+    def create_jobs
+      GTFS::Realtime::Configuration.all.each do |config|
+
+
+        create_file "app/jobs/#{config.name.gsub(' ', '').downcase}_job.rb", <<-FILE
+class #{config.name.gsub(' ', '')}Job
+  def perform
+    GTFS::Realtime.refresh_realtime_feed!(GTFS::Realtime::Configuration.find_by(name: '#{config.name}'), false)
+  end
+end
+        FILE
+      end
+    end
+
+    def setup_cronotab
+      GTFS::Realtime::Configuration.all.each do |config|
+        append_to_file 'config/cronotab.rb', <<-RUBY
+Crono.perform(#{config.name.gsub(' ', '')}Job).every #{config.interval_seconds}.seconds
+        RUBY
+      end
+    end
+  end
+end
