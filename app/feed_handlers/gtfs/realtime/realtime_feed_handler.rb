0879690491fdb80471200623323eb91d09a18e4b
mathmerized <mathmerized@gmail.com>
2019-09-06 14:44:25 -0400

    bad setting of new trip updates - need trip update object not database attributes

--- app/feed_handlers/gtfs/realtime/realtime_feed_handler.rb
+++ app/feed_handlers/gtfs/realtime/realtime_feed_handler.rb
@@ -123,7 +123,7 @@ module GTFS
           # so we only need to look for new trip updates that weren't in the previously processed feed
           # and add them and their corresponding stop updates
           prev_trip_updates = prev_trip_updates_feed.entity
-          new_trip_updates = trip_updates.map{|x| trip_update_to_database_attributes(x)} - prev_trip_updates.map{|x| trip_update_to_database_attributes(x)}
+          new_trip_updates = trip_updates.select{|x| !(prev_trip_updates.map{|x| trip_update_to_database_attributes(x)}.include? trip_update_to_database_attributes(x))}
 
           GTFS::Realtime::TripUpdate.create_many(
               new_trip_updates.collect do |trip_update|
